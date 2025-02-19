f1b0e6c855af0027661d1237e9d679a076775aee
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2025-02-19 15:03:45 +0200

    Add files via upload

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,25 @@
+import './Articles.css'
+
+function Articles() {
+    return (
+        <div className='articles'>
+            <article>
+                article 1
+            </article>
+            <article>
+                article 2
+            </article>
+            <article>
+                article 2
+            </article>
+            <article>
+                article 2
+            </article>
+            <article>
+                article 2
+            </article>
+        </div>
+    )
+}
+
+export default Articles
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,28 @@
 import profile_image_1 from '/profile_image_1.png'
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
 import './Hero.css'
 import SocialLinks from '../SocialLinks/SocialLinks';
+import Articles from '../Articles/Articles';
 
 function Hero() {
     return (
-        <div className="hero in-container">
-            <div className="hero-sec-left">
-                <div className='profile-image-container'>
-                    <img className='hero-profile-image' src={profile_image_1} alt="" />
+        <>
+            <div className="hero in-container">
+                <div className="hero-sec-left">
+                    <div className='profile-image-container'>
+                        <img className='hero-profile-image' src={profile_image_1} alt="" />
+                    </div>
+                    <h1>Frontend Developer</h1>
+                    <p className='hero-description'>
+                        I’m <strong>Ahmed Osama</strong>, a passionate Computer Science student at Zagazig University.
+                        I specialize in building dynamic and responsive web applications using the <strong>MERN Stack</strong> 
+                        (MongoDB, Express.js, React.js, and Node.js).
+                    </p>
+                    <p>Let’s build something amazing together!</p>
+                    <SocialLinks />
                 </div>
-                <h1>Frontend Developer</h1>
-                <p className='hero-description'>
-                    I'm <strong>Ahmed Osama</strong>, a passionate Computer Science student at Zagazig University.
-                    I specialize in building dynamic and responsive web applications using the <strong>MERN Stack</strong> 
-                    (MongoDB, Express.js, React.js, and Node.js).
-                </p>
-                <p>Let's build something amazing together!</p>
-                <SocialLinks />
             </div>
-        </div>
+            <Articles />
+        </>
     )
 }
 
